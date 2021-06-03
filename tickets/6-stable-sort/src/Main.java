f7713e1e1d0a4d4c8e54c86425326a02e04d1bb1
victor1234544 <moskvcihev_v2@gmail.com>
2021-06-03 08:22:14 +0300

    without console

---
+++ tickets/6-stable-sort/src/Main.java
@@ -0,0 +1,58 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Main {
+    public static void run(BufferedReader in, BufferedWriter out) throws IOException {
+        String line;
+        HashMap<Long, ArrayList<String>> map = new HashMap<>();
+        while ((line = in.readLine()) != null) {
+            String[] lexemes = line.split(" ");
+            switch (lexemes[0]) {
+                case "add" -> {
+                    Long ind = Long.parseLong(lexemes[1]);
+                    map.putIfAbsent(ind,new ArrayList<>());
+                    map.get(ind).add(lexemes[2]);
+                }
+                case "remove" -> map.remove(Long.parseLong(lexemes[1]));
+                case "print" -> {
+                    map.forEach((ind, value) -> value.forEach(
+                            it -> {
+                                try {
+                                    out.write(ind + " " + it + "\n");
+                                } catch (IOException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                    ));
+                    out.newLine();
+                }
+                default -> throw new IllegalStateException("Unexpected value: " + lexemes[0]);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        switch (args.length) {
+            case 0 -> {
+                //TODO
+            }
+            case 1 -> {
+                //TODO
+            }
+            case 2 -> {
+                try(BufferedReader reader = new BufferedReader(new FileReader(args[0]))) {
+                    try(BufferedWriter writer = new BufferedWriter(new FileWriter(args[1]))) {
+                        run(reader,writer);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            default -> System.err.println("Must be 2");
+        }
+    }
+
+}
