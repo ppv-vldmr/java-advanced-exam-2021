5ceed59a70842691257f5affe16b27398239ea48
victor1234544 <moskvcihev_v2@gmail.com>
2021-06-04 22:06:37 +0300

    works with Exception

--- tickets/6-stable-sort/src/Main.java
+++ tickets/6-stable-sort/src/Main.java
@@ -4,11 +4,16 @@ import java.util.HashMap;
 
 public class Main {
     static BufferedWriter out;
+
+    static void print_err(String str) {
+        System.out.println(str);
+    }
+
     static void print(String str) throws IOException {
-        if(out!=null) {
-            out.write(str+"\n");
+        if (out != null) {
+            out.write(str + "\n");
         } else {
-            System.out.print(str);
+            System.out.println(str);
         }
     }
 
@@ -17,47 +22,54 @@ public class Main {
         HashMap<Long, ArrayList<String>> map = new HashMap<>();
         while ((line = in.readLine()) != null) {
             String[] lexemes = line.split(" ");
-            switch (lexemes[0]) {
-                case "add" -> {
-                    Long ind = Long.parseLong(lexemes[1]);
-                    map.putIfAbsent(ind,new ArrayList<>());
-                    map.get(ind).add(lexemes[2]);
-                }
-                case "remove" -> map.remove(Long.parseLong(lexemes[1]));
-                case "print" -> {
-                    map.forEach((ind, value) -> value.forEach(
+            try {
+                switch (lexemes[0]) {
+                    case "add" -> {
+                        Long ind = Long.parseLong(lexemes[1]);
+                        map.putIfAbsent(ind, new ArrayList<>());
+                        map.get(ind).add(lexemes[2]);
+                    }
+                    case "remove" -> map.remove(Long.parseLong(lexemes[1]));
+                    case "print" -> map.forEach((ind, value) -> value.forEach(
                             it -> {
                                 try {
-                                    print(ind + " " + it+"\n");
+                                    print(ind + " " + it);
                                 } catch (IOException e) {
                                     e.printStackTrace();
                                 }
                             }
                     ));
+                    default -> throw new IllegalStateException("Unexpected value: " + lexemes[0]);
                 }
-                default -> throw new IllegalStateException("Unexpected value: " + lexemes[0]);
+            } catch (ArrayIndexOutOfBoundsException e) {
+                print_err("was less arguments then expected");
+            } catch (NumberFormatException e) {
+                print_err("error parsing number " + lexemes[1]);
             }
         }
     }
 
+
     public static void main(String[] args) {
         switch (args.length) {
             case 0 -> {
-                try(BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
-                        run(reader);
+                try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+                    run(reader);
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
             }
             case 2 -> {
-                try(BufferedReader reader = new BufferedReader(new FileReader(args[0]))) {
-                    try(BufferedWriter writer = new BufferedWriter(new FileWriter(args[1]))) {
+                try (BufferedReader reader = new BufferedReader(new FileReader(args[0]))) {
+                    try (BufferedWriter writer = new BufferedWriter(new FileWriter(args[1]))) {
                         out = writer;
                         run(reader);
                     } catch (IOException e) {
+                        System.err.println("Problems with output file");
                         e.printStackTrace();
                     }
                 } catch (IOException e) {
+                    System.err.println("Problems with input file");
                     e.printStackTrace();
                 }
             }
