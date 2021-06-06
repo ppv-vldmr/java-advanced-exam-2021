f75fee6f922e9ed8f683db1f32b262dae67d1f1b
victor1234544 <moskvcihev_v2@gmail.com>
2021-06-07 01:49:55 +0300

    code

--- tickets/ticket-25/src/ToDoList.java
+++ tickets/ticket-25/src/ToDoList.java
@@ -1,19 +1,27 @@
 import java.io.*;
-import java.nio.file.InvalidPathException;
-import java.util.HashMap;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class ToDoList {
-    HashMap<String, String> uncompletedTasks, completedTasks;
+    HashMap<String, String> incompleteTasks, completeTasks;
 
-    void print(String arg, PrintStream out) {
+    static Map<String, MODE> to = new HashMap<>();
+
+    enum MODE {INCOMPLETE, COMPLETE, ALL}
+
+    static {
+        to.put("incomplete", MODE.INCOMPLETE);
+        to.put("complete", MODE.COMPLETE);
+        to.put("all", MODE.ALL);
+    }
+
+    void print(MODE arg, PrintStream out, boolean mark) {
         switch (arg) {
-            case "uncompleted" -> {
-                out.println("Uncompleted :");
-                uncompletedTasks.forEach((label, task) -> out.format("\t%s : %s\n", label, task));
-            }
-            case "completed" -> {
-                out.println("Completed : ");
-                completedTasks.forEach((label, task) -> out.format("\t%s : %s\n", label, task));
+            case INCOMPLETE -> incompleteTasks.forEach((label, task) -> out.format("* %s : %s\n", label, task));
+            case COMPLETE -> completeTasks.forEach((label, task) -> out.format(mark ? "* ~~%s : %s~~\n" : "* %s : %s\n", label, task));
+            case ALL -> {
+                print(MODE.COMPLETE, out, true);
+                print(MODE.INCOMPLETE, out, true);
             }
             default -> System.err.format("Unsupported label - %s\n", arg);
         }
@@ -24,47 +32,56 @@ public class ToDoList {
             String line;
             String[] lexemes;
             while ((line = in.readLine()) != null) {
-                lexemes = line.split(" ");
-                try {
+                try{
+                    lexemes = line.split(" ");
                     switch (lexemes[0]) {
-                        case "load" -> {
-                        }
-                        case "unload" -> {
-                            PrintStream out = new PrintStream(new FileOutputStream(lexemes[1]));
-                            print("uncompleted",out);
-                            print("completed",out);
-                        }
                         case "add" -> {
-                        }
-                        case "mark" -> {
+                            if (lexemes.length < 3) {
+                                try (BufferedReader fileIn = new BufferedReader(new FileReader(lexemes[1]))) {
+                                    String record;
+                                    while ((record = fileIn.readLine()) != null) {
+                                        String[] words = record.split("/* ~~| : |~~|/* ");
+                                        if (record.endsWith("~~")) completeTasks.put(words[0], words[1]);
+                                        else incompleteTasks.put(words[0], words[1]);
+                                    }
+                                }
+                            } else {
+                                incompleteTasks.put(lexemes[1], Arrays.stream(lexemes).skip(2).collect(Collectors.joining(" ")));
+                            }
 
                         }
+                        case "mark" -> {
+                            if (lexemes.length == 1) System.err.format("Must be two words");
+                            String task;
+                            if ((task = incompleteTasks.remove(lexemes[1])) != null) {
+                                completeTasks.put(lexemes[1], task);
+                            }
+                        }
                         case "delete" -> {
+                            if (lexemes.length == 1) System.err.format("Must be two words");
+                            completeTasks.remove(lexemes[1]);
+                            incompleteTasks.remove(lexemes[1]);
                         }
                         case "print" -> {
-                            if (lexemes.length == 1) System.err.format("Must be two words");
-                            else if (lexemes[1].equals("all")) {
-                                print("uncompleted",System.out);
-                                print("completed",System.out);
-                            } else print(lexemes[1],System.out);
+                            PrintStream out = System.out;
+                            if (lexemes.length == 3) out = new PrintStream(new FileOutputStream(lexemes[2]));
+                            print(to.get(lexemes[1]), out, false);
                         }
                         default -> System.err.format("Unsupported operation : %s\n", lexemes[0]);
                     }
-                } catch (ArrayIndexOutOfBoundsException e) {
-                    System.err.format("must be more words\n");
-                } catch (InvalidPathException e) {
-                    System.err.format("Incorrect path : %s \n %s\n", lexemes[1], e.getReason());
+                }catch (ArrayIndexOutOfBoundsException e){
+                    System.err.println("Must be more words");
                 }
             }
         } catch (IOException e) {
             System.err.println("Problems with input :");
             e.printStackTrace();
-        }
+        } catch (NullPointerException ignored) {}
     }
 
     public ToDoList() {
-        completedTasks = new HashMap<>();
-        uncompletedTasks = new HashMap<>();
+        completeTasks = new HashMap<>();
+        incompleteTasks = new HashMap<>();
     }
 
     public static void main(String[] args) {
