356969e679137cec6ef215304bd62b14d9d112dc
victor1234544 <moskvcihev_v2@gmail.com>
2021-06-06 17:27:33 +0300

    update readme and skeleton

--- tickets/ticket-25/src/ToDoList.java
+++ tickets/ticket-25/src/ToDoList.java
@@ -1,11 +1,23 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.nio.file.InvalidPathException;
 import java.util.HashMap;
 
 public class ToDoList {
     HashMap<String, String> uncompletedTasks, completedTasks;
+
+    void print(String arg, PrintStream out) {
+        switch (arg) {
+            case "uncompleted" -> {
+                out.println("Uncompleted :");
+                uncompletedTasks.forEach((label, task) -> out.format("\t%s : %s\n", label, task));
+            }
+            case "completed" -> {
+                out.println("Completed : ");
+                completedTasks.forEach((label, task) -> out.format("\t%s : %s\n", label, task));
+            }
+            default -> System.err.format("Unsupported label - %s\n", arg);
+        }
+    }
 
     void run() {
         try (BufferedReader in = new BufferedReader(new InputStreamReader(System.in))) {
@@ -18,14 +30,23 @@ public class ToDoList {
                         case "load" -> {
                         }
                         case "unload" -> {
+                            PrintStream out = new PrintStream(new FileOutputStream(lexemes[1]));
+                            print("uncompleted",out);
+                            print("completed",out);
                         }
                         case "add" -> {
                         }
                         case "mark" -> {
+
                         }
                         case "delete" -> {
                         }
                         case "print" -> {
+                            if (lexemes.length == 1) System.err.format("Must be two words");
+                            else if (lexemes[1].equals("all")) {
+                                print("uncompleted",System.out);
+                                print("completed",System.out);
+                            } else print(lexemes[1],System.out);
                         }
                         default -> System.err.format("Unsupported operation : %s\n", lexemes[0]);
                     }
