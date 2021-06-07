559333f9693dee837753b39c422564b7dc4ff30e
Vladimir <47123587+ppv-vldmr@users.noreply.github.com>
2021-06-08 01:35:27 +0300

    test preparing

---
+++ tickets/ticket-7/src/Test.java
@@ -0,0 +1,22 @@
+public class Test {
+
+    @org.junit.Test
+    static void sameClasses() {
+
+    }
+
+    @org.junit.Test
+    static void listAndArrayList() {
+
+    }
+
+    @org.junit.Test
+    static void arrayListAndList() {
+
+    }
+
+    @org.junit.Test
+    static void listAndCollection() {
+
+    }
+}
