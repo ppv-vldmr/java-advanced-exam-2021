eb8d5717aeab6ec248cd17e319487143f00e5883
victor1234544 <moskvcihev_v2@gmail.com>
2021-06-13 00:39:03 +0300

    stub Sort.java

--- tickets/ticket-29/src/Sort.java
+++ tickets/ticket-29/src/Sort.java
@@ -1,2 +1,16 @@
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.util.Comparator;
+
 public class Sort {
+    static {
+    }
+
+    Sort(Comparator<String> comp, InputStream in, PrintStream err, PrintStream out) {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
