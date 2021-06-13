12837a510784d51b64e461ae0918ed5aa5ce306d
VladSG <vladislav.sg@yandex.ru>
2021-06-13 20:40:39 +0300

    init ticket-02

---
+++ tickets/ticket-02/src/Server.java
@@ -0,0 +1,13 @@
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.util.List;
+
+public interface Server extends Remote {
+    void addString(String s) throws RemoteException;
+
+    boolean deleteLastString() throws RemoteException;
+
+    boolean deleteString(String s) throws RemoteException;
+
+    List<String> getAllStrings() throws RemoteException;
+}
