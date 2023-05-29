106980578c0a8e43f61e5208a233ced51bdc664f
Angry Advanced Alpaka <29694517+advancedAlpaka@users.noreply.github.com>
2023-05-29 21:23:35 +0300

    Ticket 21 (#33)
    * README.md
    
    * from zachet
    
    ---------
    
    Co-authored-by: victor1234544 <moskvcihev_v2@gmail.com>

---
+++ tickets/ticket-21/src-other/Cockroach.java
@@ -0,0 +1,10 @@
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class Cockroach {
+    public void run(List<Integer> road) {
+        Arrays.sort(road.toArray());
+    }
+}
